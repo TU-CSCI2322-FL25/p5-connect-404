4614130ae68484685c51e7e77b54e6353fa5d0d8
Matthew Bledsoe <mbledso1@trinity.edu>
2025-11-07 14:47:08 -0600

    story 1 done for real

--- connect.hs
+++ connect.hs
@@ -2,6 +2,6 @@ type GameState = (Board, Color)
 type Move = int 
 type Board = [Column] 
 data Piece = Empty | Full Color deriving (Show, Eq)
-data Winner = Won Player | NoWinner deriving (Show, Eq)
+data Winner = Won Player | NotWin Player | TieWin Player deriving (Show, Eq)
 type Column = [Pieces] 
 data Color = Red | Yellow deriving (Show, Eq)

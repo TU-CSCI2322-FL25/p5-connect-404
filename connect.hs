dba93a2b632bb7cf76bc98a34208e6b39eccc7db
Matthew Bledsoe <mbledso1@trinity.edu>
2025-11-08 13:43:10 -0600

    story 5 sideways

--- connect.hs
+++ connect.hs
@@ -1,7 +1,43 @@
 type GameState = (Board, Color) 
-type Move = int 
+type Move = Int 
 type Board = [Column] 
 data Piece = Empty | Full Color deriving (Show, Eq)
-data Winner = Won Player | NotWin Player | TieWin Player deriving (Show, Eq)
-type Column = [Pieces] 
+data Winner = Won Color | NotWin Color | TieWin Color deriving (Show, Eq)
+type Column = [Piece] 
 data Color = Red | Yellow deriving (Show, Eq)
+
+
+pieceToString :: Piece -> String
+pieceToString Empty         = "Empty  "
+pieceToString (Full Red)    = "Red    "
+pieceToString (Full Yellow) = "Yellow "
+
+columnToString :: Column -> String
+columnToString []     = ""
+columnToString (x:xs) = pieceToString x ++ columnToString xs
+
+boardToString :: Board -> String
+boardToString [] = ""
+boardToString (x:xs) = columnToString x ++ "\n" ++ boardToString xs
+
+
+
+
+emptyColumn :: Column
+emptyColumn = replicate 6 Empty  
+
+fullColumn :: Column
+fullColumn = replicate 6 (Full Red)  
+
+halfFullColumn :: Column
+halfFullColumn = Full Red : replicate 5 Empty  
+
+oneFullBoard :: Board
+oneFullBoard = fullColumn : fullColumn : halfFullColumn : replicate 4 emptyColumn
+
+emptyBoard :: Board
+emptyBoard = replicate 7 emptyColumn  
+
+gameStart :: GameState
+gameStart = (emptyBoard, Red)
+

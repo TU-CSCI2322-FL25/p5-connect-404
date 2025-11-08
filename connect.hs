b80b662595634717152cf3d4d3bb8f776a05c873
Matthew Bledsoe <mbledso1@trinity.edu>
2025-11-08 13:06:26 -0600

    Story 4 done, not tested

--- connect.hs
+++ connect.hs
@@ -5,3 +5,10 @@ data Piece = Empty | Full Color deriving (Show, Eq)
 data Winner = Won Color | NotWin Color | TieWin Color deriving (Show, Eq)
 type Column = [Piece] 
 data Color = Red | Yellow deriving (Show, Eq)
+
+legalMoves :: GameState -> [Move]
+legalMoves gs@(board,color) = [fst x | x <- makeLookupList board, Empty `elem` snd x]
+
+
+makeLookupList :: Board -> [(Int, Column)]
+makeLookupList board = zip [0..6] board

23650901625ab71e7be044d8c6cbe1668e30adc7
Matthew-Bledsoe <mbledso1@trinity.edu>
2025-11-12 14:47:26 -0600

    fixing main branch had issues with merge but good now

--- connect.hs
+++ connect.hs
@@ -6,6 +6,45 @@ data Winner = Won Color | NotWin Color | TieWin Color deriving (Show, Eq)
 type Column = [Piece] 
 data Color = Red | Yellow deriving (Show, Eq)
 
+legalMoves :: GameState -> [Move]
+legalMoves gs@(board,color) = [fst x | x <- makeLookupList board, Empty `elem` snd x]
+
+makeLookupList :: Board -> [(Int, Column)]
+makeLookupList board = zip [0..6] board
+
+--Story 3 compute the result of a legal move
+
+updateGame :: GameState -> Move -> GameState
+
+updateGame gs@(board,color) move = (newBoard, opponentColor color)
+    where
+        newBoard = (updateBoard move board color)
+
+
+
+
+
+updateBoard ::  Move ->  Board -> Color -> Board
+updateBoard 0 [x] color     = (updateColumn x color) :[]
+updateBoard 0 (x:xs) color  = (updateColumn x color) :xs
+updateBoard move (x:xs) color = x:updateBoard (move - 1) xs color
+
+
+
+--updateColumn
+
+updateColumn :: Column -> Color -> Column 
+updateColumn (Empty:xs) color = Full color : xs
+updateColumn (x:xs) color = x : updateColumn xs color
+
+
+
+--returns opposite color
+
+opponentColor :: Color -> Color
+opponentColor Red = Yellow
+opponentColor Yellow = Red
+
 
 pieceToString :: Piece -> String
 pieceToString Empty         = "Empty  "

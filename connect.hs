2f41fcf4b541fca8bfadee107c8b73f94604f4f3
JC <scidiva12@gmail.com>
2025-11-12 14:45:03 -0600

    created a scratchpad for us to play around in.

--- connect.hs
+++ connect.hs
@@ -1,20 +1,20 @@
-type GameState = (Board, Color) 
+type GameState = (Board, Color, Winner) 
 type Move = Int 
 type Board = [Column] 
-data Piece = Empty | Full Color deriving (Show, Eq)
-data Winner = Won Color | NotWin Color | TieWin Color deriving (Show, Eq)
+data Piece = Empty | Piece Color deriving (Show, Eq)
+data Winner = Won Color | NotWinner | Tie deriving (Show, Eq)
 type Column = [Piece] 
 data Color = Red | Yellow deriving (Show, Eq)
 
 legalMoves :: GameState -> [Move]
-legalMoves gs@(board,color) = [fst x | x <- makeLookupList board, Empty `elem` snd x]
+legalMoves gs@(board,color, winner) = [fst x | x <- makeLookupList board, Empty `elem` snd x]
 
 
 makeLookupList :: Board -> [(Int, Column)]
 makeLookupList board = zip [0..6] board
 --Story 3 compute the result of a legal move
 updateGame :: GameState -> Move -> GameState
-updateGame gs@(board,color) move = (newBoard, opponentColor color)
+updateGame gs@(board,color, winner) move = (newBoard, opponentColor color, winner)
     where
         newBoard = (updateBoard move board color)
 
@@ -26,7 +26,7 @@ updateBoard move (x:xs) color = x:updateBoard (move - 1) xs color
 
 --updateColumn
 updateColumn :: Column -> Color -> Column 
-updateColumn (Empty:xs) color = Full color : xs
+updateColumn (Empty:xs) color = Piece color : xs
 updateColumn (x:xs) color = x : updateColumn xs color
 
 --returns opposite color
@@ -43,16 +43,17 @@ emptyColumn :: Column
 emptyColumn = replicate 6 Empty  
 
 fullColumn :: Column
-fullColumn = replicate 6 (Full Red)  
+fullColumn = replicate 6 (Piece Red)  
 
+--Half full means 3 peieces, the previous only had one. It was misleading. Fixed it. 
 halfFullColumn :: Column
-halfFullColumn = Full Red : replicate 5 Empty  
+halfFullColumn = replicate 3 (Piece Red) ++ replicate 3 Empty
 
 oneFullBoard :: Board
 oneFullBoard = fullColumn : fullColumn : halfFullColumn : replicate 4 emptyColumn
 
 gameStart :: GameState
-gameStart = (oneFullBoard, Red)
+gameStart = (oneFullBoard, Red, Won Red)
 
 emptyBoard :: Board
 emptyBoard = replicate 7 emptyColumn  
@@ -61,8 +62,6 @@ emptyBoard = replicate 7 emptyColumn
 --gameStart = (emptyBoard, Red)
 
 gameStart2 = updateGame gameStart 4
-
-
 
 
 

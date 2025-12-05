bb8999ba2301defea75d935778a471804d92f37d
Cyrus Orbell <corbell@trinity.edu>
2025-12-05 14:49:30 -0600

    interactive start

--- InputOutput.hs
+++ InputOutput.hs
@@ -44,16 +44,17 @@ pieceToStr (Full Yellow) = Just 'Y'
 pieceToStr (Full Red) = Just 'R'
 pieceToStr Empty = Just 'E'
 -- story 22-27 flags: 
-data Flag = Winner | Depth String | Help | Move String | Verbose deriving (Eq, Show)
+data Flag = Winner | Depth String | Help | Move String | Verbose | Interactive deriving (Eq, Show)
 
 
 options :: [OptDescr Flag]
 options =
-  [ Option ['w'] ["winner"]    (NoArg Winner)          "Prints the best move for a given game",
-    Option ['d'] ["depth"]     (ReqArg Depth "<#>")    "Allows the user to specify <#> as a cutoff depth.",
-    Option ['h'] ["help"]      (NoArg Help)            "Print usage information and exit.",
-    Option ['m'] ["move"]      (ReqArg Move "<#>")     "Makes the move at <#> and returns the resulting board to stdout ",
-    Option ['v'] ["verbose"]   (NoArg Verbose)            "Outputs both the move and a description of how good it is: win, lose, tie, or a rating."
+  [ Option ['w'] ["winner"]      (NoArg Winner)          "Prints the best move for a given game",
+    Option ['d'] ["depth"]       (ReqArg Depth "<#>")    "Allows the user to specify <#> as a cutoff depth.",
+    Option ['h'] ["help"]        (NoArg Help)            "Print usage information and exit.",
+    Option ['m'] ["move"]        (ReqArg Move "<#>")     "Makes the move at <#> and returns the resulting board to stdout ",
+    Option ['v'] ["verbose"]     (NoArg Verbose)         "Outputs both the move and a description of how good it is: win, lose, tie, or a rating.",
+    Option ['i'] ["interactive"] (NoArg Interactive)     "Outputs both the move and a description of how good it is: win, lose, tie, or a rating."
 
   ]
 --story 14: IO functions 
@@ -65,15 +66,21 @@ main = do
 
     if not (null errors) || Help `elem` flags
         then printHelp opts 
-       
+    
+    else if null flags 
+        then runDepth [Depth "8"] nonFlags -- this is default for story 21
+
     else if Winner `elem` flags 
         then runWinner nonFlags
         
     else if  hasDepth flags 
-        then runDepth flags
+        then runDepth flags nonFlags
 
     else if hasMove flags 
         then runMove flags nonFlags
+    
+    else if hasInteractive flags
+        then runInteractive flags nonFlags
 
     else putStrLn "Invalid arguments"
 
@@ -86,8 +93,6 @@ printHelp (flags, inputs, errors) =
      putStrLn $ usageInfo "Fortunes [options] [files]" options
 
 
-
-
 runWinner nonFlags = do
     let file = head nonFlags
     contents <- readFile file
@@ -97,11 +102,16 @@ runWinner nonFlags = do
     print move
 
 
-runDepth flags = do 
+runDepth flags nonFlags = do 
     let depth = getDepth flags
+    --file = head nonFlags 
+    --contents <- readFile
+    --let
+        --game = readGame contents
+        --results@(rating move) = whoMightWin game depth
+    --print rating ++ move
     print depth -- this is for test
-            -- use depth for a funtion that doesnt exist yet and get result and print like winner
-
+    
 
 runMove flags nonFlags = do
     let file = head nonFlags
@@ -118,9 +128,11 @@ runMove flags nonFlags = do
     putStrLn gameString
 
 
+runInteractive flags nonFlags = do
+    putStrLn "interactive"
+----------------------------------------------
 
 
-    
 getDepth :: [Flag] -> Int
 getDepth [] = 4 --default idk if i shold do this or use a Maybe
 getDepth (Depth str:_) = read str
@@ -132,7 +144,7 @@ getMove (Move str:_) = read str
 getMove (_:flags) = getMove flags
 
 
--- i got this from chatgpt but it was the same idea i was trying 
+-- i got this from chatgpt but it was the same idea i was trying basically just `elem` for flags with strings
 hasDepth :: [Flag] -> Bool
 hasDepth = any isDepth
   where
@@ -150,5 +162,11 @@ hasVerbose = any isVerbose
   where
     isVerbose (Verbose) = True
     isVerbose _         = False
+
+hasInteractive :: [Flag] -> Bool
+hasInteractive = any isInteractive
+  where
+    isInteractive (Interactive) = True
+    isInteractive _         = False
 
     
